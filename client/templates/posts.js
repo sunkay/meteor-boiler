66bc440a0f425a244b5df96af0f76c0990bcd57a
sunkay <sunkay11@gmail.com>
2015-02-17 22:52:14 -0500

    added useraccounts package

--- client/templates/posts.js
+++ client/templates/posts.js
@@ -1,25 +1,5 @@
-var postsData = [ 
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  }, {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  },
-    {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-    {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function(){
+    return Posts.find();
+  }
 });
---
+++ server/fixtures.js
@@ -0,0 +1,12 @@
+if (Posts.find().count() === 0) { Posts.insert({
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  });
+  Posts.insert({
+    title: 'Meteor',
+    url: 'http://meteor.com'
+});
+  Posts.insert({
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+}); }
