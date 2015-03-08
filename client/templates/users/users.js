0618705d9b2b7b3c27f06cc4fed9ec6cb49e65d2
sunkay <sunkay11@gmail.com>
2015-03-08 17:27:52 -0400

    created users list page & admin can delete users

---
+++ client/templates/users/users.js
@@ -0,0 +1,35 @@
+
+Template.userList.helpers({
+	allusers: function(){
+		return UserList.find();
+	}
+});
+
+Template.userItem.events({
+	'click .deleteUser': function(e){
+		e.preventDefault();
+
+		log.info("In Delete User");
+
+		var user = this;
+		bootbox.confirm("Are you sure?", function(result){
+			if(!result){
+				FlashMessages.sendSuccess("Whew!! not deleted :-)");
+				return;
+			}
+
+			Meteor.call('deleteUserById', user._id, 
+				function (error) {
+					if(error){
+						log.error(error);
+						FlashMessages.sendError(error.reason);
+					} else {
+						FlashMessages.sendSuccess("User has been deleted");
+					}
+				});
+
+
+			Router.go("/users");
+		});		
+	}
+});
